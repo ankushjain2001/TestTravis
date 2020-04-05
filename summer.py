e58a5220737351a3ada74d43fecb8ff80125ba21
Ankush Jain <ankushjain2001@yahoo.co.in>
2020-04-05 12:27:46 -0500

    Update summer.py

--- summer.py
+++ summer.py
@@ -1,6 +1,5 @@
 # Imports imports but doesn't know what to do with the imports
 import numpy
-import pandas
 
 def summer(a,b):
   # Return a+b but it won't and why should it? It shon't and it won't, though it not con't.
